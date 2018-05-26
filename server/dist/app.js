95c34b7bbf78dadcb9ad02fafa6f61da1c249080
dani7cl <daniel19.cl@gmail.com>
2018-05-26 16:58:20 +0200

    dist

---
+++ server/dist/app.js
@@ -0,0 +1,24 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const masterRouter = require("./routes/index");
+class App {
+    constructor() {
+        this.express = express();
+        this.express.use(bodyParser.urlencoded({
+            extended: false
+        }));
+        this.mountRoutes();
+    }
+    mountRoutes() {
+        this.express.use(express.static(path.join(__dirname, 'client-dist')));
+        this.express.use('/api', masterRouter);
+        this.express.use((req, res) => {
+            res.sendFile(path.join(__dirname, './client-dist/index.html'));
+        });
+    }
+}
+exports.default = new App().express;
+//# sourceMappingURL=app.js.map
